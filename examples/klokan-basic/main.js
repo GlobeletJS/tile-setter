57738f9fdbd81d3cd156f51937c1d7730ba0f43f
Jeshurun Hembd <jjhembd@gmail.com>
2020-09-26 17:21:39 -0500

    Update tile-gl version, and fix klokan-basic example

--- examples/klokan-basic/main.js
+++ examples/klokan-basic/main.js
@@ -1,31 +1,30 @@
 import * as yawgl from 'yawgl';
 import * as d3 from 'd3';
 import * as vectorMap from "../../dist/vector-map.bundle.js";
-import * as projection from "../../src/proj-mercator.js";
 
 export function main() {
   const canvas = document.getElementById("mapCanvas");
+  yawgl.resizeCanvasToDisplaySize(canvas, window.devicePixelRatio);
+
   const gl = yawgl.getExtendedContext(canvas);
 
-  const style = "klokantech-basic-style.json";
-
-  vectorMap.init({ gl, style })
-    .promise.then(api => setup(api, canvas))
+  vectorMap.init({ 
+    gl,
+    center: [-73.885, 40.745],
+    zoom: 9,
+    style: "./klokantech-basic-style.json", 
+  }).promise.then(setup)
     .catch(console.log);
 }
 
-function setup(api, canvas) {
-  const viewport = [canvas.clientWidth, canvas.clientHeight];
+function setup(api) {
+  const viewport = api.getViewport(window.devicePixelRatio);
+  const canvas = api.gl.canvas;
 
-  const initTransform = getTransform({ 
-    center: [-73.885, 40.745], 
-    zoom: 9, 
-    viewport
-  });
-
+  const { k, x, y } = api.getTransform();
   var transform = d3.zoomIdentity
-    .translate(initTransform.x, initTransform.y)
-    .scale(initTransform.k);
+    .translate(x, y)
+    .scale(k);
 
   const zoomer = d3.zoom()
     .scaleExtent([1 << 10, 1 << 26])
@@ -42,27 +41,13 @@ function setup(api, canvas) {
 
   //api.draw(transform);
   //api.when("tileLoaded", () => api.draw(transform));
-  //const ctx = canvas.getContext("2d");
 
   requestAnimationFrame(animate);
   function animate(time) {
-    //ctx.drawImage(api.canvas, 0, 0);
-    //setTimeout(() => {
     let pixRatio = window.devicePixelRatio;
     let resized = yawgl.resizeCanvasToDisplaySize(canvas, pixRatio);
-    api.draw(transform, pixRatio);
+    api.setTransform(transform);
+    api.draw(pixRatio);
     requestAnimationFrame(animate);
-    //});
   }
-}
-
-function getTransform({ center: [lon, lat], zoom, viewport }) {
-  const degrees = 180 / Math.PI;
-
-  let k = 512 * 2 ** zoom;
-  let [x, y] = projection
-    .lonLatToXY([], [lon / degrees, lat / degrees])
-    .map((c, i) => (0.5 - c) * k + viewport[i] / 2);
-
-  return { k, x, y };
 }
