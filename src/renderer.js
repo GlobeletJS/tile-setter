f910aa166a23db029ece9fed5b9147479768bc59
Jeshurun Hembd <jjhembd@gmail.com>
2020-11-09 19:28:41 -0600

    Use updated tile-painter to do the rendering loop over tiles

--- src/renderer.js
+++ src/renderer.js
@@ -5,11 +5,8 @@ export function initRenderer(context, style) {
   const { sources, spriteData: spriteObject, layers } = style;
 
   const painters = layers.map(layer => {
-    let source = sources[layer.source];
-    let tileSize = source ? source.tileSize : 512;
-
-    let painter = initMapPainter({
-      context, spriteObject, tileSize,
+    let painter = initMapPainter({ 
+      context, spriteObject, 
       styleLayer: getStyleFuncs(layer),
     });
 
@@ -17,41 +14,14 @@ export function initRenderer(context, style) {
     return painter;
   });
 
-  function drawLayers(tilesets, zoom, pixRatio = 1) {
+  return function(tilesets, zoom, pixRatio = 1) {
     context.bindFramebufferAndSetViewport();
     context.clear();
     painters.forEach(painter => {
       if (zoom < painter.minzoom || painter.maxzoom < zoom) return;
       if (!painter.visible()) return;
-      drawLayer(painter, zoom, tilesets[painter.source], pixRatio);
+      let tileset = tilesets[painter.source];
+      painter({ tileset, zoom, pixRatio });
     });
-  }
-
-  function drawLayer(painter, zoom, tileset, pixRatio) {
-    // No tiles for background layers
-    if (!tileset) return painter({ zoom });
-
-    let { translate: [tx, ty], scale } = tileset;
-    let pixScale = scale * pixRatio;
-
-    for (const tileBox of tileset) {
-      if (!tileBox) continue;
-
-      let position = {
-	x: (tileBox.x + tx) * pixScale,
-	y: (tileBox.y + ty) * pixScale,
-	w: pixScale,
-      };
-
-      painter({
-	source: tileBox.tile.data,
-	position,
-	crop: { x: tileBox.sx, y: tileBox.sy, w: tileBox.sw },
-	zoom,
-	pixRatio,
-      });
-    }
-  }
-
-  return drawLayers;
+  };
 }
